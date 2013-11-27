b8c8b28a6e056c1831f0a711a47e8ea585c8a358
chinna gaadu <ajay.edap@gmail.com>
2013-11-27 08:38:16 +0530

    -

---
+++ Java/Lab/HttpRequest/src/talk/to/irctc/PNRStatus.java
@@ -0,0 +1,13 @@
+package talk.to.irctc;
+
+public class PNRStatus {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Java/Lab/HttpRequest/src/talk/to/irctc/TrainBerthAvailability.java
@@ -0,0 +1,17 @@
+package talk.to.irctc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TrainBerthAvailability {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Map<String,String> formData=new HashMap<String,String>();
+		
+
+	}
+
+}
